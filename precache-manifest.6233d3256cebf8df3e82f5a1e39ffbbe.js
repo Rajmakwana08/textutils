b4514e2db56a1a1dcc0467d92b0b974e2aa9442c
Rajmakwana08 <rajrajmakwana08@gmail.com>
2024-11-19 00:01:22 -0800

    Updates

---
+++ precache-manifest.6233d3256cebf8df3e82f5a1e39ffbbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5061535ae6e978bb33d7e3a5df01db2",
+    "url": "/textutils/index.html"
+  },
+  {
+    "revision": "e0e1b5c1d502432bd73d",
+    "url": "/textutils/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "c36854e9f1ce5730035a",
+    "url": "/textutils/static/js/2.517e4975.chunk.js"
+  },
+  {
+    "revision": "67ae3e0212c3a537d86a1f099983ebeb",
+    "url": "/textutils/static/js/2.517e4975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5770b6dbafba458b69",
+    "url": "/textutils/static/js/3.9d25297e.chunk.js"
+  },
+  {
+    "revision": "e0e1b5c1d502432bd73d",
+    "url": "/textutils/static/js/main.c1a8b58c.chunk.js"
+  },
+  {
+    "revision": "be3a5429e865666ce8e7",
+    "url": "/textutils/static/js/runtime-main.28099f2f.js"
+  }
+]);
